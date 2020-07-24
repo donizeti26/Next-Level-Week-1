1b3908f5ec58a8ee5d104bf28d78a38f551df02b
donizeti26 <silva.donizeti26@gmail.com>
2020-07-24 12:51:42 -0300

    Instalaçao do nunjucks, organizaçao das pastas, extends e inputs nas 3 paginas criaçao de server e configuraçao de atalhos no packge.json, inserçao de variaveis pageId, inserçao do layout e correçao de formulario(responsividade).

---
+++ src/server.js
@@ -0,0 +1,32 @@
+//criando servidor
+const express = require("express") //usando o express instalado
+const server = express()
+
+//configurar pasta publica
+server.use(express.static("public"))
+
+//utilizando tamplete engine
+
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+//configurar caminhos da minha aplicação
+//pagina inicial
+//req: requisição
+//res: resposta
+server.get("/",(req,res)=>{
+   return res.render("index.html",{title:"Um titulo"})
+})
+
+server.get("/create-point",(req,res)=>{
+    return res.render("create-point.html")
+})
+server.get("/search",(req,res)=>{
+    return res.render("search-results.html")
+})
+
+//ligar o servidor
+server.listen(3000)
